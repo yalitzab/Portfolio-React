36644c3e5d0f88ec2924762e1999de384cc92828
yalitzab <yalitzabueno@gmail.com>
2021-02-15 19:34:15 -0800

    update CSS

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import { validateEmail } from '../../utils/helpers';
 
-function ContactForm() {
+function Contact() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
   const [errorMessage, setErrorMessage] = useState('');
@@ -63,4 +63,4 @@ function ContactForm() {
   );
 }
 
-export default ContactForm;
+export default Contact;
